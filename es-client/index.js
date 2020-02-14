b004650f5cb5a5a80aedddef7eccb9aa2900a3d3
Jarosław Bagnicki <jarek@bagnicki.net>
2020-02-14 05:19:44 +0100

    Add elasticsearch node client to dependencies

--- es-client/index.js
+++ es-client/index.js
@@ -1,11 +1,16 @@
 const express = require('express');
+const { Client } = require('@elastic/elasticsearch')
 
 const PORT = process.env.PORT || 3000;
+const ES_HOST = process.env.ES_HOST || 'localhost'
+const ES_PORT = process.env.ES_PORT || 9200
 
 const server = express();
+const client = new Client({ node: `http://${ES_HOST}:${ES_PORT}` })
 
 server.get('/', (req, res) => {
     const dt = new Date();
+    client;
     res.send(`${dt.toISOString()}: Hello from Node.js`);
 });
 
